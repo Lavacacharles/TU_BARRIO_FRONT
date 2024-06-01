596022113effe63b69299e522bd7133bd6428fc0
Juan Leibniz <142361563+Lavacacharles@users.noreply.github.com>
2024-06-01 09:29:08 -0500

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+
+class Tubarrio11Widget extends StatefulWidget {
+@override
+_Tubarrio11WidgetState createState() => _Tubarrio11WidgetState();
+}
+
+class _Tubarrio11WidgetState extends State<Tubarrio11Widget> {@override
+    Widget build(BuildContext context) {
+    // Figma Flutter Generator Tubarrio11Widget - RECTANGLE
+
+        return Container(
+            width: 894,
+            height: 1464,
+            decoration: BoxDecoration(
+                image : DecorationImage(
+                    image: AssetImage('assets/images/Tubarrio11.png'),
+                    fit: BoxFit.fitWidth
+                ),
+            )
+        );
+    }
+}
+
+class Tubarrio21Widget extends StatelessWidget {@override
+    Widget build(BuildContext context) {
+    // Figma Flutter Generator Tubarrio21Widget - RECTANGLE
+    return Container(
+        width: 860,
+        height: 1728,
+        decoration: BoxDecoration(
+            image : DecorationImage(
+                image: AssetImage('assets/images/Tubarrio21.png'),
+                fit: BoxFit.fitWidth
+                ),
+            )
+        );
+    }
+}
+
+
+class Tubarrio31Widget extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+        // Figma Flutter Generator Tubarrio31Widget - RECTANGLE
+        return Container(
+            width: 851,
+            height: 1544,
+            decoration: BoxDecoration(
+                image : DecorationImage(
+                    image: AssetImage('assets/images/Tubarrio31.png'),
+                    fit: BoxFit.fitWidth
+                    ),
+                )
+            child: AssetImage('assets/images/Tubarrio31.png')
+        );
+    }
+    }
+        
